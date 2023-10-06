28b983f0fdf2271bf5e2fcb69a0922ccb0938b1e
MargaretTechworld <MargaretTechworld@gmail.com>
2023-10-06 01:21:08 +0000

    Add TrimmerDecorator class

---
+++ trimmer_decorator.rb
@@ -0,0 +1,8 @@
+require_relative 'base_decorator'
+
+# class for TrimmerDecorator
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
+  end
+end
