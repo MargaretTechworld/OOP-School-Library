fe65510a28d7a58df8134b593b9c770a025876d9
joyce <joyce.ann.cruz@gmail.com>
2023-10-09 16:28:06 +0800

    Refactor main.rb

--- main.rb
+++ main.rb
@@ -1,13 +1,6 @@
 require_relative 'app'
 require_relative 'options'
 
-def show_options
-  puts "\nPlease choose and option by entering a number:
-1 - List all books\n2 - List all people\n3 - Create a person
-4 - Create a book\n5 - Create a rental
-6 - List all rentals for a given person id\n7 - Exit"
-end
-
 def prompt
   puts 'Welcome to School Library App!'
   loop do
@@ -18,5 +11,3 @@ def prompt
     call_option(option)
   end
 end
-
-main
